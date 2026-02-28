a67c1cc7ee831a60e3fc20b6ed0cf2d72b5ac5db
Deniz <dennizatar@gmail.com>
2026-02-28 18:46:17 +0000

    NearbyNeutralAgent completed, and fully functional.

---
+++ Planet Wars Source Code/planet-wars-rts-main/planet-wars-rts-main/app/bin/main/games/planetwars/agents/self/NearbyNeutralAgent.kt
@@ -0,0 +1,44 @@
+package games.planetwars.agents.self
+
+import games.planetwars.agents.Action
+import games.planetwars.agents.PlanetWarsPlayer
+import games.planetwars.agents.random.CarefulRandomAgent
+import games.planetwars.core.GameParams
+import games.planetwars.core.GameState
+import games.planetwars.core.GameStateFactory
+import games.planetwars.core.Player
+import games.planetwars.runners.GameRunner
+import games.planetwars.view.GameView
+import xkg.jvm.AppLauncher
+
+class NearbyNeutralAgent() : PlanetWarsPlayer() {
+    override fun getAction(gameState: GameState): Action {
+        // filter the planets that are owned by the player AND have a transporter available
+        val myPlanets = gameState.planets.filter { it.owner == player && it.transporter == null }
+        if (myPlanets.isEmpty()) {
+            return Action.doNothing()
+        }
+        // now find a random target planet not owned by the player or the opponent
+        val neutralPlanets = gameState.planets.filter { it.owner == Player.Neutral }
+        if (neutralPlanets.isEmpty()) {
+            return Action.doNothing()
+        }
+        val source = myPlanets.random()
+        // PHASE 2: Target the closest neutral planet using minByOrNull
+        val target = neutralPlanets.minByOrNull { it.position.distance(source.position) }!!
+        return Action(playerId = player, sourcePlanetId = source.id, destinationPlanetId = target.id, numShips = source.nShips/2)
+    }
+
+    override fun getAgentType(): String {
+        return "Nearby Neutral Agent"
+    }
+}
+
+
+fun main() {
+    val agent = NearbyNeutralAgent()
+    agent.prepareToPlayAs(Player.Player1, GameParams())
+    val gameState = GameStateFactory(GameParams()).createGame()
+    val action = agent.getAction(gameState)
+    println(action)
+}
